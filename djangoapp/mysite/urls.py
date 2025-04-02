f4a1772becba07b1ab281db3f2295bcfae98601f
abhi <abhishekgavhane231@gmail.com>
2025-04-02 21:28:08 +0800

    hello feature

--- djangoapp/mysite/urls.py
+++ djangoapp/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse 
+
+def hello(request):
+    return HttpResponse("Hello from django app")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
 ]
